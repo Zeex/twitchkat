6a4a3b5fe92eddb8b048c2614ee860cf1ba6eda5
Zeex <zeex@rocketmail.com>
2020-03-15 15:22:49 +0600

    First commit

---
+++ app/src/main/java/io/github/zeex/twitch4oa/MainActivity.kt
@@ -0,0 +1,39 @@
+package io.github.zeex.twitch4oa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoRuntimeSettings
+import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.GeckoSessionSettings
+import org.mozilla.geckoview.GeckoView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val webView = findViewById<GeckoView>(R.id.web_view)
+        val sessionSettings = GeckoSessionSettings.Builder()
+            .viewportMode(GeckoSessionSettings.VIEWPORT_MODE_DESKTOP)
+            .userAgentMode(GeckoSessionSettings.USER_AGENT_MODE_DESKTOP)
+            .build()
+        val session = GeckoSession(sessionSettings)
+        val runtimeSettings = GeckoRuntimeSettings.Builder()
+            .aboutConfigEnabled(false)
+            .consoleOutput(false)
+            .debugLogging(false)
+            .inputAutoZoomEnabled(false)
+            .preferredColorScheme(GeckoRuntimeSettings.COLOR_SCHEME_DARK)
+            .remoteDebuggingEnabled(false)
+            .build()
+        val runtime = GeckoRuntime.create(this, runtimeSettings)
+        
+        session.open(runtime)
+        webView.setSession(session)
+        session.loadUri("https://www.twitch.tv")
+    }
+}
